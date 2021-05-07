ebe6c6b946c9224f5f9a3822d81179cc41cdccbc
Giovanni Rivera <grivera64@toromail.csudh.edu>
2021-05-07 04:00:12 -0700

    :tada: Initial Commit

---
+++ readline.h
@@ -0,0 +1,14 @@
+/*
+
+    Name: readline.h
+    Purpose: Is the header file to access the readline.c file
+    Author: grivera64
+    Date: 04/07/2021
+
+*/
+
+/* Defines the read_line() function from readline.c */
+#ifndef READLINE_H
+#define READLINE_H 
+    int read_line(char str[], int n);
+#endif
